6eb25f23e2c943cf11e5ed28d71f21962b2dcfae
cryptokat <crypto.kat@semux.io>
2018-05-27 13:42:28 +0800

    Add 'GET /supply-summary' API

--- api/app/Providers/AppServiceProvider.php
+++ api/app/Providers/AppServiceProvider.php
@@ -15,6 +15,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
+        define('SEM_NANO', bcpow(10, 9));
+        define('PREMINE_SEM', '25000000');
+        define('DELEGATE_FEE_SEM', '1000');
+        define('ADDRESS_AIRDROPS', "0x1504263ee17446ea5f8b288e1c35d05749c0e47d");
+        define('ADDRESS_DEV', "0x541365fe0818ea0d2d7ab7f7bc79f719f5f72227");
+        define('ADDRESS_FOUNDERS', "0xe30c510f3efc6e2bf98ff8f725548e6ece568f89");
+
         bcscale(9);
 
         $this->app->singleton(SemuxApi::class, function ($app) {
--- api/routes/web.php
+++ api/routes/web.php
@@ -12,6 +12,7 @@
 */
 
 use Psr\Http\Message\ServerRequestInterface;
+use Illuminate\Contracts\Cache\Repository as Cache;
 use Semux\Client\Api\SemuxApi;
 
 $router->get('/', function () use ($router) {
@@ -23,19 +24,78 @@ $router->group(['prefix' => 'api'], function() use ($router) {
 		return response()->json(['version' => "1.2.0"]);
 	});
 
-	$router->get('/circulating-supply', function (ServerRequestInterface $request, SemuxApi $api) use ($router) {
+	// provided for crypto trackers
+	$router->get('/circulating-supply', function (SemuxApi $api) use ($router) {
 		$latestBlockNumber = $api->getLatestBlockNumber()->getResult();
-		$blockRewards = bcmul($latestBlockNumber, 3);
+		$blockRewardsSEM = bcmul($latestBlockNumber, 3);
 
 		$delegates = $api->getDelegates()->getResult();
-		$burnedCoins = bcmul(sizeof($delegates), "1000");
+		$burnedCoinsSEM = bcmul(sizeof($delegates), DELEGATE_FEE_SEM);
 
-		$premine = "25000000";
-
-		return bcsub(bcadd($blockRewards, $premine), $burnedCoins);
+		return bcsub(bcadd($blockRewardsSEM, PREMINE_SEM), $burnedCoinsSEM);
 	});
 
-	$router->get('/total-supply', function () use ($router) {
-		return "100000000";
-	});
+	// supply summary
+    $router->get('/supply-summary', function (SemuxApi $api, Cache $cache) use ($router) {
+        // total supply
+        $maxSEM = "100000000";
+
+        // community dist
+        $airdropsSEM = $cache->remember("airdropsSEM", 60, function () use ($api) {
+            $airdropsResp = $api->getAccount(ADDRESS_AIRDROPS)->getResult();
+            return bcdiv(bcadd($airdropsResp->getAvailable(), $airdropsResp->getLocked()), SEM_NANO);
+        });
+
+        // development
+        $devSEM = $cache->remember('devSEM', 60, function () use ($api) {
+            $devResp = $api->getAccount(ADDRESS_DEV)->getResult();
+            return bcdiv(bcadd($devResp->getAvailable(), $devResp->getLocked()), SEM_NANO);
+        });
+
+        // founders
+        $foundersSEM = $cache->remember('foundersSEM', 60, function () use ($api) {
+            $foundersResp = $api->getAccount(ADDRESS_FOUNDERS)->getResult();
+            return bcdiv(bcadd($foundersResp->getAvailable(), $foundersResp->getLocked()), SEM_NANO);
+        });
+
+        // block rewards
+        $blockRewardsSEM = $cache->remember('blockRewardsSEM', 1, function () use ($api) {
+            return bcmul($api->getLatestBlockNumber()->getResult(), 3);
+        });
+
+        // burned coins
+        $burnedCoinsSEM = $cache->remember('burnedCoinsSEM', 60, function () use ($api) {
+            return bcmul(sizeof($api->getDelegates()->getResult()), DELEGATE_FEE_SEM);
+        });
+
+        // locked coins
+        $lockedCoinsSEM = $cache->remember('lockedCoinsSEM', 60, function () use ($api) {
+            $delegates = $api->getDelegates()->getResult();
+            $locked = '0';
+            foreach ($delegates as $delegate) {
+                $locked = bcadd($locked, $delegate->getVotes());
+            }
+            return bcdiv($locked, SEM_NANO);
+        });
+
+        $distributedPremine = bcsub(PREMINE_SEM, bcadd(bcadd($airdropsSEM, $devSEM), $foundersSEM));
+
+        $availableCommunityCoins = bcsub(bcadd($distributedPremine, $blockRewardsSEM), $burnedCoinsSEM);
+
+        return response()->json([
+            'maximumSupply' => $maxSEM,
+            'totalPremine' => PREMINE_SEM,
+            'totalSupply' => bcsub(bcadd($blockRewardsSEM, PREMINE_SEM), $burnedCoinsSEM),
+            'availablePremine' => [
+                'airdrops' => $airdropsSEM,
+                'development' => $devSEM,
+                'founders' => $foundersSEM
+            ],
+            'distributedPremine' => $distributedPremine,
+            'blockRewards' => $blockRewardsSEM,
+            'burnedCoins' => $burnedCoinsSEM,
+            'lockedCoins' => $lockedCoinsSEM,
+            'availableCommunityCoins' => $availableCommunityCoins
+        ]);
+    });
 });
